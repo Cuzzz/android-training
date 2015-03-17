aa893e83adecbd25eb619e780671e2103c73ecf0
Ivan Zhukov <ivan.zhukov@umbrella-web.com>
2015-03-18 02:28:04 +0300

    Compiled

--- app/src/main/java/com/example/engineer/test02/MainActivity.java
+++ app/src/main/java/com/example/engineer/test02/MainActivity.java
@@ -4,16 +4,73 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button calcBtn;
+
+    private ArrayList<String> history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calcBtn = (Button) findViewById(R.id.calc_btn);
+
+        history = new ArrayList<>();
+
+
+        //View.OnClickListener btnCalcLsn = ;
+
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcResult();
+            }
+        });
+
     }
 
+
+    public void calcResult()
+    {
+        EditText input_a = (EditText) findViewById(R.id.input_a);
+        EditText input_b = (EditText) findViewById(R.id.input_b);
+        TextView result_a_b;
+        result_a_b = (TextView) findViewById(R.id.result_a_b);
+
+        String a = input_a.getText().toString();
+        String b = input_b.getText().toString();
+
+        Integer result = Integer.parseInt(a) + Integer.parseInt(b);
+
+        result_a_b.setText(result.toString());
+
+        history.add(a+"+"+b+"="+result.toString());
+        outHistory();
+    }
+
+    public void outHistory()
+    {
+        TextView history_text = (TextView) findViewById(R.id.history_text);
+
+        String hist="";
+
+        for (int i=history.size()-1; i>=0;i--)
+        {
+            hist+=history.get(i)+"\n";
+        }
+
+        history_text.setText(hist);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
